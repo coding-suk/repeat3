d774f638a24674a1b029e597f6a8007b2eac7646
developer1 <dev1@email.com>
2024-09-10 11:35:02 +0900

    first commit

---
+++ src/main/java/com/web/sundragon3/Sundragon3Application.java
@@ -0,0 +1,13 @@
+package com.web.sundragon3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sundragon3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sundragon3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/sundragon3/Sundragon3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.sundragon3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sundragon3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
